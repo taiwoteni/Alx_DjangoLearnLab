25e886cf7a6976cd499b36eadc596889f6f7aeef
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2025-07-21 07:31:15 +0100

    Finished Week 10 Task 3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import login
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books
 from .views import LibraryDetailView
 from . import views
@@ -6,4 +8,7 @@ from . import views
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,25 @@
 from .models import Library
-from django.views.generic.detail import DetailView
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from .models import Book
+from django.views.generic.detail import DetailView
 
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# User registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 
 # Class-based view to display library details
 class LibraryDetailView(DetailView):
