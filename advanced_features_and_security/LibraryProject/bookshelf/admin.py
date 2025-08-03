835385735a6180e9dc6c48bea42998a550e061d5
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2025-08-03 19:54:08 +0100

    Finished Week 11 Task 1

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,70 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import CustomUser, Book
-
-
-class CustomUserAdmin(UserAdmin):
-    """
-    Custom admin interface for CustomUser model.
-    """
-    
-    # Fields to display in the user list view
-    list_display = ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff', 'is_active', 'date_joined')
-    
-    # Fields that can be searched
-    search_fields = ('username', 'first_name', 'last_name', 'email')
-    
-    # Fields that can be filtered
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_joined', 'last_login')
-    
-    # Fields to display when editing a user
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {
-            'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')
-        }),
-        (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
-        }),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-    
-    # Fields to display when adding a new user
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2'),
-        }),
-        (_('Personal info'), {
-            'classes': ('wide',),
-            'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo'),
-        }),
-        (_('Permissions'), {
-            'classes': ('wide',),
-            'fields': ('is_active', 'is_staff', 'is_superuser'),
-        }),
-    )
-    
-    # Read-only fields
-    readonly_fields = ('date_joined', 'last_login')
-    
-    # Ordering
-    ordering = ('username',)
-    
-    # Custom methods to display in the admin
-    def get_full_name(self, obj):
-        """Display the user's full name."""
-        return obj.get_full_name() or '-'
-    get_full_name.short_description = _('Full Name')
-    
-    def get_age(self, obj):
-        """Display the user's age."""
-        return obj.age or '-'
-    get_age.short_description = _('Age')
-    
-    # Add custom methods to list display
-    list_display = list_display + ('get_full_name', 'get_age')
+from .models import Book, CustomUser
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -77,11 +14,40 @@ class BookAdmin(admin.ModelAdmin):
     ordering = ('title',)
 
 
-# Register the models with their respective admin classes
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Book, BookAdmin)
+class CustomUserAdmin(UserAdmin):
+    """
+    Admin interface for CustomUser model.
+    """
+    model = CustomUser
+    
+    # Fields to display in the admin list view
+    list_display = ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'age', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_of_birth')
+    
+    # Fields to search in admin
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    
+    # Fieldsets for the admin form
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    # Fields for adding a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
+        }),
+    )
+    
+    ordering = ('username',)
 
-# Customize admin site headers
-admin.site.site_header = _('Library Project Administration')
-admin.site.site_title = _('Library Project Admin')
-admin.site.index_title = _('Welcome to Library Project Administration')
+
+# Register the models
+admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 
 class CustomUserManager(BaseUserManager):
