d2c48151d7f3fa13f6a30a8c76a8b1a9c42c786e
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2025-08-17 22:47:14 +0100

    Finished Week 14 Task 2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('login')  # Redirect to login page after registration
+    else:
+        form = LoginForm()
+    return render(request, 'register.html', {'form': form})
+
+from django.contrib.auth.models import User
+
+@login_required
+def profile_view(request):
+    user = request.user
+    return render(request, 'profile.html', {'user': user})
